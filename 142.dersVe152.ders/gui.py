db1d8d1195f838d4bfad53eb43471ab25a831593
Deniz Denizcan <denizcnn4@gmail.com>
2025-01-17 21:00:47 +0300

    Create basic GUI without interactions,day 16

---
+++ 142.dersVe152.ders/gui.py
@@ -0,0 +1,16 @@
+import functions
+import FreeSimpleGUI
+
+
+label = FreeSimpleGUI.Text("Type in a to-do")
+input_box = FreeSimpleGUI.InputText(tooltip="Enter todo")
+add_button = FreeSimpleGUI.Button("Add")
+
+
+window = FreeSimpleGUI.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
+
+# Window içine pencerenin aldığı değerleri ekleriz.
+#layout her zaman liste bekler.Layout da iç köşeli parantezin içine koyduğumuz ögeler bir satıra yerleştirilir.
+#eğer layout içine yazdıkların ayrı ayrı satırlarda gözüksün istiyorsan her birini köşeli paranteze al.
