e7c634d11ad9e977bcc02b640e4a59f5c43b2590
Deniz Denizcan <denizcnn4@gmail.com>
2025-01-19 15:15:00 +0300

    Implement the Edit button

--- 142.dersVe152.ders/gui.py
+++ 142.dersVe152.ders/gui.py
@@ -2,23 +2,40 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key="to-do")  # to-do bu giriş kutusunun kimlğidir.
+input_box = sg.InputText(tooltip="Enter todo", key="todo")  # to-do bu giriş kutusunun kimlğidir.
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=(45, 10))           #mevcut yapılcakların listesini verir.
+edit_button = sg.Button("Edit")
+
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
-                   font=('Helvetica', 20))  # font yazı tipi ve büuüklüğünü ayarlar.
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   font=('Helvetica', 20))        # font yazı tipi ve büuüklüğünü ayarlar.
 
 while True:
     event, values = window.read()  # window.read metodu bir tuple döndürür.event values iki ögeli tuple olarak döndürür.
-    print(event)  # Bir buttona basmak bir olaydır.ve event basılan button un etiketini alır.
-    print(values)  # values değişkeni kullanıcı tarafından doldurulan değerleri alır.
+    print(1, event)  # Bir buttona basmak bir olaydır.ve event basılan button un etiketini alır.
+    print(2, values)  # values değişkeni kullanıcı tarafından doldurulan değerleri alır.
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values["to-do"] + "\n"
+            new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)   #edit ettiğin todos hemen listbox içinde güncel görünmesi için update function kullandık.
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
@@ -48,3 +65,4 @@ Genel Kullanım
 Olay kontrolü için: Hangi düğmeye basıldığını ya da pencerenin kapanıp kapanmadığını belirler.
 Kullanıcı girdilerini almak için: Değerler sözlüğünden erişilir.
 """
+#35. satır ListBox'ı günceller, 37. satır ise düzenlenmek üzere seçilen görevi metin kutusuna aktarır.
