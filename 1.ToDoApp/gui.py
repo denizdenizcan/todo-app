4887d3d2758cb9f110b1b9633758ff5fcef9c4e7
Deniz Denizcan <denizcnn4@gmail.com>
2025-01-19 23:22:33 +0300

    Implement the Complete button and Exit button

--- 1.ToDoApp/gui.py
+++ 1.ToDoApp/gui.py
@@ -7,11 +7,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))           #mevcut yapılcakların listesini verir.
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=('Helvetica', 20))        # font yazı tipi ve büuüklüğünü ayarlar.
+                   layout=[[label],
+                          [input_box, add_button],
+                          [list_box, edit_button, complete_button],
+                          [exit_button]],
+                   font=('Helvetica', 20))          # font yazı tipi ve büuüklüğünü ayarlar.
 
 while True:
     event, values = window.read()  # window.read metodu bir tuple döndürür.event values iki ögeli tuple olarak döndürür.
@@ -34,6 +38,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)   #edit ettiğin todos hemen listbox içinde güncel görünmesi için update function kullandık.
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
